df00d5d3c1c2b5c4a024f4ed777be7253471230c
reubenjsinclair <reubenjsinclair@gmail.com>
2022-02-25 12:37:33 +0000

    Add error for too large a sum

--- src/index.js
+++ src/index.js
@@ -1,17 +1,140 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Row extends React.Component{
+
+  render(){
+    return(
+      <div className="exam">
+        <p>Percentage worth:  </p>
+        <input 
+          type="number" 
+          min="0"
+          max="100"
+          defaultValue="100"
+          onChange={e=>this.props.changeWorth(this.props.id,e.target.value)}/>
+        <p>Percentage got in the exam:  </p>
+        <input 
+          type="number" 
+          min="0"
+          max="100"
+          defaultValue="100"
+          onChange={e=>this.props.changeScore(this.props.id,e.target.value)}/>
+        <button onClick={e=>this.props.delete(this.props.id)}>remove</button>
+      </div>
+    );
+  }
+}
+
+class Main extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      worths:[],
+      scores:[],
+      rows:[],
+      current:0,
+      active:[]
+    };
+    this.handleScoreChange = this.handleScoreChange.bind(this);
+    this.handleWorthChange = this.handleWorthChange.bind(this);
+    this.delete = this.delete.bind(this);
+    this.doCalc = this.doCalc.bind(this);
+  }
+
+  handleScoreChange(i,newScore){
+    const oldScores=this.state.scores;
+    oldScores[i]=newScore;
+    this.setState({
+      scores: oldScores 
+    })
+  };
+
+  handleWorthChange(i,newWorth){
+    const oldWorths=this.state.worths;
+    oldWorths[i]=newWorth;
+    this.setState({
+      worths: oldWorths 
+    })
+  };
+
+  delete(i){
+    this.setState({
+      active: this.state.active.filter(w=>w!==i),
+    })
+  }
+
+  renderRow(i){
+    return(
+      <Row 
+        id={i} 
+        worth={this.state.worths[i]} 
+        score={this.state.scores[i]}
+        changeScore={this.handleScoreChange}
+        changeWorth={this.handleWorthChange}
+        delete={this.delete}
+      />
+    )
+  }
+
+  addRow(){
+    this.setState({
+      worths: this.state.worths.concat(100),
+      scores: this.state.scores.concat(100),
+      rows: this.state.rows.concat(this.renderRow(this.state.current)),
+      current: this.state.current+1,
+      active: this.state.active.concat(this.state.current)
+    });
+
+  }
+
+  doCalc(i){
+    return this.state.worths[i]*this.state.scores[i]/100
+  }
+
+  getSum(){
+    const currentWorths=this.state.active.map((row)=>parseInt(this.state.worths[row]))
+    console.log(currentWorths);
+
+    return Math.round(currentWorths.reduce((prev,current)=>prev+current,0),2);
+
+  }
+
+  calc(){
+    const halfCalc = this.state.active.map(this.doCalc)
+    return halfCalc.reduce((prev,current)=>prev+current,0);
+  }
+
+  render() {
+    const rows = this.state.rows
+    const showRows = rows.map((row)=><div key={row.props.id}>{row}</div>)
+    const showRowsCont=this.state.active.map((i)=>showRows[i]);
+    let message;
+    if(this.getSum()>100){
+      message=<h2 className="error">Your percentages sum to more than 100</h2>
+    } else {
+      message=<h2>Currently, you are on: {this.calc()}%</h2>
+    }
+    return (
+      <div>
+        <div>
+          {showRowsCont}
+        </div>
+        <div>
+          <button onClick={()=>this.addRow()}>hey!</button>
+        </div>
+        <div>
+          {message} 
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Main />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
